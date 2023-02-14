718492539b67b3cca4b557815195af126382e373
Juhani Koski <juhkukoskii@gmail.com>
2023-02-14 20:56:49 +0200

    v2

--- W1/Calc1/app/src/main/java/com/example/calc1/MainActivity.kt
+++ W1/Calc1/app/src/main/java/com/example/calc1/MainActivity.kt
@@ -13,11 +13,29 @@ class MainActivity : AppCompatActivity() {
         val firstVal:EditText=findViewById(R.id.etFirstNum)
         val secondVal:EditText=findViewById(R.id.etSecondNum)
         val sumButton:Button=findViewById(R.id.buSum)
+        val minButton:Button=findViewById(R.id.buMin)
+        val mulButton:Button=findViewById(R.id.buMul)
+        val divButton:Button=findViewById(R.id.buDiv)
         val resultTv:TextView=findViewById(R.id.tvResult)
         sumButton.setOnClickListener {
             val first=firstVal.text.toString().toDouble()
             val second=secondVal.text.toString().toDouble()
-            resultTv.text=(first+second).toString()
+            resultTv.text=String.format("%.2f",first+second)
+        }
+        minButton.setOnClickListener {
+            val first=firstVal.text.toString().toDouble()
+            val second=secondVal.text.toString().toDouble()
+            resultTv.text=String.format("%.2f",first-second)
+        }
+        mulButton.setOnClickListener {
+            val first=firstVal.text.toString().toDouble()
+            val second=secondVal.text.toString().toDouble()
+            resultTv.text=String.format("%.2f",first*second)
+        }
+        divButton.setOnClickListener {
+            val first=firstVal.text.toString().toDouble()
+            val second=secondVal.text.toString().toDouble()
+            resultTv.text=String.format("%.2f",first/second)
         }
     }
 }
